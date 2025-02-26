57620daa572359635c8a7c03147054569198cca9
rofa <roghaye.fallahi75@gmail.com>
2025-02-26 13:52:03 +0330

    updated

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -4,7 +4,7 @@ function Layout({ children }) {
   return (
     <>
       <header className="font-vazir flex justify-between items-center text-white mt-5 mx-0 mb-10">
-        <h2 className="text-2xl">سیستم مدیریت مشتریان</h2>
+        <h2 className="font-bold">سیستم مدیریت مشتریان</h2>
         <Link
           href="/add-customer"
           className="bg-[#8af1aa]  text-[#262836] px-[10px] py-[5px] font-bold rounded-md border-2 border-solid border-[#8af1aa]"
